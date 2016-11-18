8f9144e479c0ecd9dd161f8738f1c4379fc0a8f4
JP Kabler <john.kabler@gmail.com>
2016-11-18 13:36:16 -0700

    adding accountUtils
    first push in

---
+++ App/src/utils/accountUtils.js
@@ -0,0 +1,45 @@
+//Accounts menu functions
+const populateAccountsList = (store) => {
+  const token = store.accessToken
+  const fetchAccounts = getAcccountsAjaxCall(token);
+
+  const parseAccounts = (results) => {
+    const accounts = results.items;
+    const accountsList = accounts.map((d, i) => {
+      return {uiobject: d.name, dataname: d.id}
+    });
+    return accountsList;
+  }
+
+  const setAccountsDropdownMenu = (accountsList) => {
+    //Alteryx.Gui.manager.GetDataItem('accountsDropDown').setStringList(accountsList);
+    store.accountsList.stringList = accountsList;
+    console.log(accountsList);
+  }
+
+  fetchAccounts
+    .then(parseAccounts)
+    .done(setAccountsDropdownMenu)
+    //.fail(handleApiFail)
+}
+
+const getAcccountsAjaxCall = (accessToken) => {
+    const auth = "Bearer " + accessToken
+    const url = "https://www.googleapis.com/analytics/v3/management/accounts"
+
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": url,
+        "method": "GET",
+        "dataType": "json",
+        "headers": {
+            "authorization": auth,
+            "cache-control": "no-cache",
+        }
+    };
+
+    return $.ajax(settings)
+};
+
+export { populateAccountsList } 
