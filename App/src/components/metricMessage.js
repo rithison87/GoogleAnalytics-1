3a7f7a92bd985ede3628437515b9ee735e75ac5f
Stephen Wagner <stephen.wagner.a@gmail.com>
2016-11-30 14:37:08 -0600

    metricMessage.js - refactored the render
    Refactored the render for metricMessage.js

--- App/src/components/metricMessage.js
+++ App/src/components/metricMessage.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-
-const Metric = ( { metricName }) => <button type="button" className="selectionMessage-btn">{metricName}</button>;
-
-function MetricList( { selections }) {
-  return (
-    <div>
-      {
-        selections.map((selection, idx) => <Metric key={idx} metricName={selection} />)
-      }
-    </div>
-  );
-}
+import _ from 'lodash'
+import * as metrics from '../utils/metrics'
 
 class MetricMessage extends React.Component {
   constructor(props) {
@@ -19,9 +9,20 @@ class MetricMessage extends React.Component {
     this.store = props.store;
   }
 
+  // clicked(item) {
+  //   console.log(item)
+  //   store.metricsList.selection.splice(item,1);
+  // }
+
   render() {
+    let selections = this.store.metricsList.selection
     return (
-      <MetricList selections={this.store.metricsList.selection} />
+      <div>
+        {
+          // onClick={() => this.clicked(idx)}
+          selections.map((selection, idx) => <button type="button" className="selectionMessage-btn">{selection}</button>)
+        }
+      </div>
     );
   }
 }
