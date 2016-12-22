0b2d42f68c14bfbfc704f367682cbbd4090fbad3
Unknown <dfrey@alteryx.com>
2016-12-22 13:43:33 -0700

    console log cleanup

--- App/src/utils/goals.js
+++ App/src/utils/goals.js
@@ -1,5 +1,5 @@
 import AyxStore from '../stores/AyxStore'
-import {toJS} from 'mobx' 
+import {toJS} from 'mobx'
 import _ from 'lodash'
 
 
@@ -7,7 +7,7 @@ const accountId = '226181'
 const webPropertyId = 'UA-226181-9'
 const profileId = '31055553'
 const goalsUri = 'https://www.googleapis.com/analytics/v3/management/accounts'
-const goalsRequestUri = 'https://www.googleapis.com/analytics/v3/management/accounts/'+accountId+'/webproperties/'+webPropertyId+'/profiles/' +profileId+'/goals' 
+const goalsRequestUri = 'https://www.googleapis.com/analytics/v3/management/accounts/'+accountId+'/webproperties/'+webPropertyId+'/profiles/' +profileId+'/goals'
 const accessToken = 'ya29.CjGkA_MNlXR6u5VIqrIuFHOfenuTrFKZ3rSkLsHRRQaqjojLP734szWNjK0CvAS-Kgxr'
 
 // get goals for all profiles
@@ -21,23 +21,23 @@ const getGoalsListAjaxCall = (accessToken) => {
         "headers": {
             "Authorization": 'Bearer ' + accessToken,
             "cache-control": "private, max-age=0, must-revalidate, no-transform",
-            "content-type": "application/json; charset=UTF-8" 
-        } 
+            "content-type": "application/json; charset=UTF-8"
+        }
     }
     return $.ajax(settings)
 }
 
 //populate Metrics Goals
 const populateMetricsGoalsList = (store) => {
-	
+
 	const fetchGoals = getGoalsListAjaxCall(store.accessToken);
-	
+
 	const parseGoals = (results) => {
-		console.log(results)
+		// console.log(results)
     const goals = results.items.filter( (d) => d.active != false )
 		const goalsList = goals.map((d) => {
       return {
-      			uiobject: d.name, 
+      			uiobject: d.name,
       			dataname: d.id,
       			type: d.type}
     })
@@ -53,10 +53,10 @@ const populateMetricsGoalsList = (store) => {
   //push to metrics goals list
   const goalsStorePush = (results) => {
     results.map( (d) => {
-        
+
         store.metricsGoalsList.stringList.push({uiobject: d.uiobject, dataname: d.dataname})
     })
-    console.log(toJS(store.metricsGoalsList.stringList) )
+    // console.log(toJS(store.metricsGoalsList.stringList) )
 }
 
   fetchGoals
@@ -64,20 +64,20 @@ const populateMetricsGoalsList = (store) => {
     .then(parseGoals)
     .then(filterMetricsGoals)
   	.done(goalsStorePush)
-  	
+
 }
 
 //populate Dimensions Goals
 const populateDimensionsGoalsList = (store) => {
-  
+
   const fetchGoals = getGoalsListAjaxCall(store.accessToken);
-  
+
   const parseGoals = (results) => {
-    console.log(results)
+    // console.log(results)
     const goals = results.items.filter( (d) => d.active != false )
     const goalsList = goals.map((d) => {
       return {
-            uiobject: d.name, 
+            uiobject: d.name,
             dataname: d.id,
             type: d.type}
     })
@@ -90,13 +90,13 @@ const populateDimensionsGoalsList = (store) => {
 
   }
 
-  //push to Dimensions goals list 
+  //push to Dimensions goals list
   const goalsStorePush = (results) => {
     results.map( (d) => {
-        
+
         store.dimensionsGoalsList.stringList.push({uiobject: d.uiobject, dataname: d.dataname})
     })
-    console.log(toJS(store.dimensionsGoalsList.stringList) )
+    // console.log(toJS(store.dimensionsGoalsList.stringList) )
 }
 
   fetchGoals
@@ -104,10 +104,10 @@ const populateDimensionsGoalsList = (store) => {
     .then(parseGoals)
     .then(filterDimensionGoals)
     .done(goalsStorePush)
-    
-}
-	
 
-	
+}
+
+
+
 
 export { populateMetricsGoalsList, populateDimensionsGoalsList };
